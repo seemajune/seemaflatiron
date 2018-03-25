4eec76d6df8062b60723b63909f51817d949b083
seemajune <seemashariat@gmail.com>
2018-03-25 17:59:31 -0400

    update site design

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,74 @@
-$( document ).ready(function() {
-  $('th').css('cursor', 'pointer');
+(function() {
+  var content = document.getElementsByClassName('content')[0];
+  var aboutSection = document.getElementsByClassName('about')[0];
+  var contactSection = document.getElementsByClassName('contact')[0];
+  var expSection = document.getElementsByClassName('experience')[0];
+  var exp = document.getElementById('experience');
+  var contact = document.getElementById('contact');
+  var about = document.getElementById('about');
 
-  $('th').on('click', function(e){
-    var listToShow = $('.' + $(this).attr('id'));
-    $(listToShow).addClass('shadow-z-1').slideToggle(1000);
+  var sections = [aboutSection, contactSection, expSection];
+
+  var onTransitionEnded = function() {
+    content.removeEventListener("transitionend", onTransitionEnded);
+    showContentSection();
+  };
+
+  var activeSection;
+
+  function showContentSection() {
+    console.log(activeSection);
+    activeAndDeactivate(activeSection);
+    content.style.visibility = 'visible';
+    content.classList.add('full');
+  }
+
+  function hideContentSection() { 
+    content.style.visibility = 'hidden';
+    content.classList.remove('full');
+  }
+
+  function hideContentIfVisible() {
+    if (content.style.visibility === 'visible') {
+      content.addEventListener("transitionend", onTransitionEnded);
+      hideContentSection();
+    }
+    else {
+      showContentSection();
+    }
+  }
+
+  function activeAndDeactivate() {
+    sections.forEach(function(section){
+      if (section === activeSection) {
+        section.classList.add('active');
+        section.classList.remove('inactive');
+      } else {
+        section.classList.remove('active');
+        section.classList.add('inactive');
+      }
+    });
+  }
+
+  about.addEventListener('click', function(){
+    if (activeSection !== aboutSection) {
+      activeSection =  aboutSection;
+      hideContentIfVisible();
+    }
   });
-});
+
+  contact.addEventListener('click', function(){
+    if (activeSection !== contactSection) {
+      activeSection = contactSection;
+      hideContentIfVisible();
+    }
+  });
+
+  exp.addEventListener('click', function(){
+    if (activeSection !== expSection) {
+      activeSection = expSection;
+      hideContentIfVisible();
+    }
+  });
+
+})();
